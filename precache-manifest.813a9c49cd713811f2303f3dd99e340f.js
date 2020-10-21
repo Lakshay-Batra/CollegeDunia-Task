a1a038a9808dfa95adb6448f20975517a5fb3b32
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-10-22 02:26:48 +0530

    Updates

---
+++ precache-manifest.813a9c49cd713811f2303f3dd99e340f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bf6ee35afa4fd98c6234e400ec4061",
+    "url": "/CollegeDunia-Task/index.html"
+  },
+  {
+    "revision": "15396902cccd44b9b978",
+    "url": "/CollegeDunia-Task/static/css/main.75f638bd.chunk.css"
+  },
+  {
+    "revision": "9b580e93c2671c5c8bf9",
+    "url": "/CollegeDunia-Task/static/js/2.88baa39c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/CollegeDunia-Task/static/js/2.88baa39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15396902cccd44b9b978",
+    "url": "/CollegeDunia-Task/static/js/main.62a0e705.chunk.js"
+  },
+  {
+    "revision": "5cd544edc27b914f313e",
+    "url": "/CollegeDunia-Task/static/js/runtime-main.b4ab0af8.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/CollegeDunia-Task/static/media/college_02.619ee97e.jpg"
+  },
+  {
+    "revision": "e9e35335ad3e7385b809db95ce60a178",
+    "url": "/CollegeDunia-Task/static/media/star-regular.e9e35335.svg"
+  },
+  {
+    "revision": "8833ab7289fd9ff241afe7afe40170cf",
+    "url": "/CollegeDunia-Task/static/media/star-solid.8833ab72.svg"
+  }
+]);
